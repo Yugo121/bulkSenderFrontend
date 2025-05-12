12ba215721685540fc5d42326f0fc20942aab1d4
Yugo121 <adam_kaszynski@tlen.pl>
2025-05-12 15:03:15 +0200

    Created categories view

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,37 +1,58 @@
 <template>
-    <h1> Category template</h1>
+    <h1 class="text-center my-3">Categories</h1>
     <div class="container">
         <div class="row mb-3">
-            <div class="col-6">
+            <div class="col-4">
             </div>
-            <div class="col-6 d-flex justify-content-end">
+            <div class="col-4">
+            </div>
+            <div class="col-4 d-flex justify-content-end">
                 <button class="btn btn-light">Get categories from baselinker</button>
             </div>
         </div>
         <div class="row mb-3">
-            <div class="col-6">
-                <span>Category name</span>
+            <div class="col-1">
             </div>
-            <div class="col-6">
-                <span>Baselinker category id</span>
+            <div class="col-12 ">
+                <table class="table table-dark table-bordered">
+                    <thead>
+                        <tr class="text-center">
+                            <th scope="col">#</th>
+                            <th scope="col">Baselinker name</th>
+                            <th scope="col">Baselinker Id</th>
+                            <th scope="col">File name</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-center" v-for="category in referenceDataStore.categories" :key="category.id">
+                        <th scope="row">{{ referenceDataStore.categories.indexOf(category) + 1}}</th>
+                        <td> {{ category.baselinkerName }} </td>
+                        <td> {{ category.baselinkerId }} </td>
+                        <td>{{ category.name }}</td>
+                        <td>
+                            <div class="d-inline-flex gap-2">
+                                <button class="btn btn-secondary btn-sm" aria-label="Edit category">Edit</button>
+                                <button class="btn btn-danger btn-sm" aria-label="Delete category">Delete</button> 
+                            </div>
+                        </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-        </div>
-        <div v-for="category in categories" :key="category.id" class="row mb-1">
-            <div class="col-6">
-                    <p class="form-control form-control-sm bg-dark text-white text-center">
-                        {{ category.name }}
-                    </p>
-            </div>
-            <div class="col-6">
-                    <p class="form-control form-control-sm bg-dark text-white text-center">
-                        {{ category.baselinkerId }}
-                    </p>
-            </div>
+            <div class="col-1"></div>
         </div>
     </div>
-
 </template>
 
 <script setup>
-let categories = [{id: 1, name: 'Category 1', baselinkerId: '123'},];
+import { useReferenceDataStore } from '@/stores/referenceDataStore.js'
+import { useModalStore } from '@/stores/modalStore';
+import { ref, onMounted } from 'vue';
+
+const referenceDataStore = useReferenceDataStore()
+
+onMounted(() => {
+    referenceDataStore.fetchCategories()
+});
 </script>
